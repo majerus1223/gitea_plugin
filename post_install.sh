62cc46317aa8910ea53bce19bc6924b00a84a391
majerus1223 <majerus1223@users.noreply.github.com>
2018-12-04 08:31:52 -0600

    Enable service

--- post_install.sh
+++ post_install.sh
@@ -3,9 +3,13 @@
 # Create user 'gitea'
 pw add user -n gitea -c Gitea -s /bin/csh -m
 
+
 #Grab port? 
 pkg install gitea
 
+#Enable Service
+sysrc -f /etc/rc.conf gitea_enable="YES"
+
 
 # Start the service
-service gitea onestart
+service gitea start
